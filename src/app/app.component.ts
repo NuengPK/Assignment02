c60f1bc2ac40d1ac6e71c69c5cd50dfaa7f6dcd7
Nueng Pasit <onetwotee1@gmail.com>
2022-02-22 19:53:51 +0700

    add  usename property Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment02';
+  txtusename = ""
+  usename = ""
+
+  onCreateusename(){
+    this.txtusename = "Usename : \""+this.usename +"\" "
+  }
 }
